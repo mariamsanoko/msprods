e5052a09f15b545653c71780297123ec24c6f23b
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-23 13:47:11 +0200

    login

---
+++ src/app/components/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const login = () => {
+    return (
+        <div>
+            <h1>Login Page</h1>
+            {/* Add your login form or buttons here */}
+        </div>
+    );
+};
+
+export default Login;
