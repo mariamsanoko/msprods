4e5fbf4ee740dd224c6c0562b04d72e187018646
mariamsanoko <mariam.sanoko@gmail.com>
2025-09-23 17:25:39 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+import Image from 'next/image'
 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <>
+      <h1>Hello, Next.js!</h1>
+      <Image src="/profile.png" alt="Profile" width={100} height={100} />
+    </>
+  );
 }
