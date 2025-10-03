b2ab9a4b414adff63a7c78ee9c2cd9f9e01fd287
mariamsanoko <mariam.sanoko@gmail.com>
2025-10-03 14:53:54 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Page() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
+      <h1>Hello, Bienvenue sur MS French Academy !</h1>
       <Image src="/profile.png" alt="Profile" width={100} height={100} />
     </>
   );
