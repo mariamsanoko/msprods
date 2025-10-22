940b8e7ea49489bddd8b6403de1d330b6dc19de1
mariamsanoko <mariam.sanoko@gmail.com>
2025-10-22 17:50:02 +0200

    I added text to the README file

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  site: 'https://creativetimofficial.github.io',
+  site: 'https://mariamsanoko.github.io/msprods',
   base: '/msprods'
 });
+
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-sticky py-3 start-0 end-0">
       <div className="container px-1">
-        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="https://www.msprods.fr">MS IA Ecommerce</a>
+        <a className="navbar-brand font-weight-bolder ms-lg-0 " href="https://www.msprods.fr">MS I.A SHOP</a>
         <button className="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon mt-2">
             <span className="navbar-toggler-bar bar1"></span>
@@ -28,8 +28,8 @@ const Navbar = () => {
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="https://www.mariamsanoko.fr/">
-                MS I.A & No-Code
+              <a className="nav-link text-dark font-weight-bold d-flex align-items-center me-2 " aria-current="page" href="h#">
+                Login
               </a>
             </li>
             <li className="nav-item">
